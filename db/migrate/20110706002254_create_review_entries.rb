7bec0b643df80ec6dd82b670454acad19b7bba11
Stephen Baldwin <stephencharlesb@gmail.com>
2011-07-11 17:52:00 -0700

    Fifth Commit

--- db/migrate/20110706002254_create_review_entries.rb
+++ db/migrate/20110706002254_create_review_entries.rb
@@ -2,7 +2,6 @@ class CreateReviewEntries < ActiveRecord::Migration
   def self.up
     create_table :review_entries do |t|
       t.string :service
-      t.string :email
       t.text :content
 
       t.timestamps
