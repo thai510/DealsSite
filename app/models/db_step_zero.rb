bc99e117d54390de4869d183658baec40cbd2062
Ian Alexander <thai510@gmail.com>
2011-09-11 19:41:47 -0700

    Added info to deal show page

--- app/models/db_step_zero.rb
+++ app/models/db_step_zero.rb
@@ -1,7 +1,7 @@
 class DbStepZero < ActiveRecord::Base
   belongs_to :user
-   has_attached_file :company_logo, :styles => { :medium => "300x300",
-                                         :thumb => "100x100" }
+   has_attached_file :company_logo, :styles => { :medium => "300x300>",
+                                         :thumb => "100x100>" }
 
   validates :industry,:business_name,:business_description, :presence => true
   validates_attachment_content_type :company_logo, :content_type => ['image/jpeg','image/png', 'image/gif']
